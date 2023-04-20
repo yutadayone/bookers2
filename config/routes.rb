8bda1d3433e1de0e05e2d467880094f74ffc39c8
yutadayone <129708410+yutadayone@users.noreply.github.com>
2023-04-20 14:05:59 +0000

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:show, :edit, :index]
+  resources :books, only: [:new, :index, :show, :edit]
+  get 'homes/top'
+  devise_for :users
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
